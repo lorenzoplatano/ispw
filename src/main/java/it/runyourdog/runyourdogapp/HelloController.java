d9c528c5e68b13f7a5f7541d3e1535ada99bdce3
lorenzo <lorenzo.platano@students.uniroma2.eu>
2025-01-16 14:09:03 +0100

    Initial commit

---
+++ src/main/java/it/runyourdog/runyourdogapp/HelloController.java
@@ -0,0 +1,14 @@
+package it.runyourdog.runyourdogapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
