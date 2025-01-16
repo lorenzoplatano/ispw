dba52d5129744fefdb4bc04963994a716c63a36d
daniele <danieleonorato02@gmail.com>
2025-01-16 17:06:43 +0100

    doglogin controller added

--- src/main/java/it/runyourdog/runyourdogapp/GraphicController/DogLoginController.java
+++
@@ -1,8 +0,0 @@
-package it.runyourdog.runyourdogapp.GraphicController;
-
-import it.runyourdog.runyourdogapp.SingletonStage;
-
-public class DogLoginController {
-    SingletonStage singStage=SingletonStage.getStage(null);
-
-}
