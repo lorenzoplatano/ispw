cccb3cba5f4b07a67b3fb6241138ed48ceb82929
daniele <danieleonorato02@gmail.com>
2025-04-24 13:56:49 +0200

    fixing

--- src/main/java/it/runyourdog/runyourdogapp/graphiccontroller/PrenotazionePadroneGraphicController.java
+++ src/main/java/it/runyourdog/runyourdogapp/graphiccontroller/PrenotazionePadroneGraphicController.java
@@ -1,15 +1,17 @@
 package it.runyourdog.runyourdogapp.graphiccontroller;
 
 import it.runyourdog.runyourdogapp.beans.ProfiloDogsitterBean;
+import it.runyourdog.runyourdogapp.beans.ProfiloPadroneBean;
 import it.runyourdog.runyourdogapp.utils.SingletonStage;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
 
 public class PrenotazionePadroneGraphicController extends GenericGraphicController {
+
     @FXML
-    protected void goToProfile(ProfiloDogsitterBean profile) throws IOException {
-        SingletonStage.getStage(null).showDogsitterHomePage("/it/runyourdog/runyourdogapp/GUI/ProfiloDogsitter.fxml", profile);
+    public void prenota() throws IOException {
+        SingletonStage.getStage(null).showPadroneHomePage("/it/runyourdog/runyourdogapp/GUI/ProfiloPadrone.fxml", (ProfiloPadroneBean) loggedUser);
     }
 
 }
