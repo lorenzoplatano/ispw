998c2102a72c7cf2985be6afbd5c403102f1901a
lorenzo <lorenzo.platano@students.uniroma2.eu>
2025-05-05 18:32:57 +0200

    fixing code smells

--- src/main/java/it/runyourdog/runyourdogapp/graphiccontroller/PadLoginGraphicController.java
+++ src/main/java/it/runyourdog/runyourdogapp/graphiccontroller/PadLoginGraphicController.java
@@ -1,23 +1,14 @@
 package it.runyourdog.runyourdogapp.graphiccontroller;
 
-
 import it.runyourdog.runyourdogapp.beans.ProfiloPadroneBean;
 import it.runyourdog.runyourdogapp.beans.UserBean;
-
 import it.runyourdog.runyourdogapp.exceptions.InvalidInputException;
 import it.runyourdog.runyourdogapp.exceptions.ProfileRetrievalException;
-import it.runyourdog.runyourdogapp.utils.*;
+import it.runyourdog.runyourdogapp.utils.SingletonStage;
 import it.runyourdog.runyourdogapp.utils.enumeration.Role;
-
-
-
-
-
 import java.io.IOException;
 
-
-
-public class PadLoginGraphicController extends GenericLoginGraphicController {
+public class PadLoginGraphicController extends GenericLoginGraphicController<ProfiloPadroneBean> {
 
     @Override
     protected Role getExpectedRole() {
@@ -25,19 +16,15 @@ public class PadLoginGraphicController extends GenericLoginGraphicController {
     }
 
     @Override
-    protected Object retrieveProfile(UserBean user) throws ProfileRetrievalException, InvalidInputException {
-
-
+    protected ProfiloPadroneBean retrieveProfile(UserBean user) throws ProfileRetrievalException, InvalidInputException {
         ProfiloPadroneBean profile = controller.getPadProfileInfo(user);
-
-
         profile.setEmail(user.getEmail());
-
         return profile;
     }
 
     @Override
-    protected void navigateToHome(Object profile) throws IOException {
-        SingletonStage.getStage(null).showPadroneHomePage("/it/runyourdog/runyourdogapp/GUI/ProfiloPadrone.fxml", (ProfiloPadroneBean) profile);
+    protected void navigateToHome(ProfiloPadroneBean profile) throws IOException {
+        SingletonStage.getStage(null).showPadroneHomePage("/it/runyourdog/runyourdogapp/GUI/ProfiloPadrone.fxml",
+                        profile);
     }
 }
