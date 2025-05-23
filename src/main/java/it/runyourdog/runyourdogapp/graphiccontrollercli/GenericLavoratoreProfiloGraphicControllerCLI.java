cd69fbfc9190363cad51142539e39e812338e1df
daniele <danieleonorato02@gmail.com>
2025-05-23 17:04:29 +0200

    fixing

--- src/main/java/it/runyourdog/runyourdogapp/graphiccontrollercli/GenericLavoratoreProfiloGraphicControllerCLI.java
+++ src/main/java/it/runyourdog/runyourdogapp/graphiccontrollercli/GenericLavoratoreProfiloGraphicControllerCLI.java
@@ -127,7 +127,7 @@ public abstract class GenericLavoratoreProfiloGraphicControllerCLI extends Gener
             String s = scanner.nextLine().trim();
             try {
                 return Time.valueOf(s + ":00");
-            } catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException error) {
                 Printer.perror("Formato orario non valido. Usa HH:mm (es. 09:30).");
             }
         }
