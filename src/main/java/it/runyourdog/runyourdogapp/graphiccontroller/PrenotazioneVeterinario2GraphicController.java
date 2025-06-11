fc2f036295b7e00d93005413d64c62160ebdf547
lorenzo <lorenzo.platano@students.uniroma2.eu>
2025-06-11 11:15:30 +0200

    final commit

--- src/main/java/it/runyourdog/runyourdogapp/graphiccontroller/PrenotazioneVeterinario2GraphicController.java
+++ src/main/java/it/runyourdog/runyourdogapp/graphiccontroller/PrenotazioneVeterinario2GraphicController.java
@@ -14,6 +14,7 @@ import it.runyourdog.runyourdogapp.utils.SingletonStage;
 import java.io.IOException;
 import java.util.List;
 
+
 public class PrenotazioneVeterinario2GraphicController extends PrenotazioneGeneric2GraphicController {
 
     @Override
